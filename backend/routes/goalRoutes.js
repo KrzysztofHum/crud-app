9cb0043966984931e94716bcae9a874cfa39a6f5
KrzysztofHum <k.humienny1996@protonmail.com>
2022-06-10 10:23:07 +0200

    create tokens

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,11 +7,13 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
+const { protect } = require("../moddleware/authMiddleware");
+
 // router.get("/", getGoals);
 // router.post("/", setGoal);
 // router.put("/:id", updateGoal);
 // router.delete("/:id", deleteGoal);
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").delete(deleteGoal).put(updateGoal);
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
